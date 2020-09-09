6f9d2a3eb197151b6dc49d6fcdb75a71c8a1186a
BhanuPrakash-LTI <70949651+BhanuPrakash-LTI@users.noreply.github.com>
2020-09-09 17:48:35 +0530

    program commit

---
+++ SimpleProgram/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            do
+            {
+                Console.WriteLine("Enter the first number");
+                 a = Convert.ToInt32(Console.ReadLine());
+
+            } while (a == 0);
+
+            do
+            {
+                Console.WriteLine("Enter the second number");
+                 b = Convert.ToInt32(Console.ReadLine());
+            }
+            while (b == 0);
+
+             int sum = a + b;
+            int product = a * b;
+
+            Console.WriteLine("The Sum is " + sum);
+            Console.WriteLine("The product is " + product);
+
+            Console.ReadKey();
+        }
+    }
+}
